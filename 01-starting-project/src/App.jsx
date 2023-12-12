978753e3c8cc436228e73a8a746408d79624d55d
Violet <hanfovab@gmail.com>
2023-12-12 18:00:34 +0100

    First project completed

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,25 +1,70 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
+
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
+
+  function handleClick(selectedBtn) {
+    setSelectedTopic(selectedBtn);
+  };
+
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    )
+  };
+
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton 
+              isSelected={selectedTopic === 'components'} 
+              onClick={() => handleClick('components')}
+              >Components</TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'jsx'} 
+              onClick={() => handleClick('jsx')}
+              >JSX</TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'props'} 
+              onClick={() => handleClick('props')}
+              >Props</TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'state'} 
+              onClick={() => handleClick('state')}
+              >State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
 };
-
-function Header() {
-  return (
-    <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
 
 export default App;
